ac62ef5e921dc3ae2f577354c9bd13e9d5821b31
Sathik basha <sathikbashamsgi@gmail.com>
2025-08-12 14:05:33 +0530

    Update Leranif.java

--- week1/day2/Leranif.java
+++ week1/day2/Leranif.java
@@ -3,30 +3,36 @@ package week1.day2;
 public class Leranif {
 
 	public static void main(String[] args) {
-		// shortcut of if ---> type "if" then press ctrl+spece
-		int marks =50;
-		if (marks>=40) {
-			System.out.println("pass");
-		}
-		if (marks>=60) {
-			System.out.println("pass");
-		}
-			else {
-				System.out.println("fail");
+	
+				//shortcut of if--->type if and do ctrl+space
+				//shortcut for comment--->ctrl+shift+/
+				int marks=50;
+				
+				  if (marks>=40) { System.out.println("pass");
+				  
+				  }
+				 
+				if (marks>=60) {
+					System.out.println("pass");
+					
+				} else {
+					System.out.println("fail");
+
+				}
+				
+				int marks1=80;
+				if (80==60) {
+					System.out.println("grade1");
+					
+				} else if(marks1<40){
+					System.out.println("fail");
+				}
+				else if(marks1>80) {
+					System.out.println("distinction");
+				}
+				else {
+					System.out.println("out of range");
+				}
 			}
-		int marks1=80;
-		if (marks1==60) {
-			System.out.println("grade1");
-		}else if (marks1<40) {
-			System.out.println("fail");
-		}else if (marks1>=80) {
-			System.out.println("distinction");
-		}
-		else {
-			System.out.println("outof range");
-		}
-			
 
-	}
-
-}
+		}
