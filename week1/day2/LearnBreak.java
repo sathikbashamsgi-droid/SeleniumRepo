b16d66dce5e2ef172617caf4102d9ea29816787e
Sathik basha <sathikbashamsgi@gmail.com>
2025-08-12 14:01:53 +0530

    Update LearnBreak.java

--- week1/day2/LearnBreak.java
+++ week1/day2/LearnBreak.java
@@ -2,16 +2,20 @@ package week1.day2;
 
 public class LearnBreak {
 
-	public static void main(String[] args) {
-		// Completely break the iteration
-		for (int i = 0; i<=10; i++) {
-			
-			if (i==5) {
+		/**
+		 * 
+		 *---it completely breaks the iteration
+		 */
+
+		public static void main(String[] args) {
+			for (int i = 0; i <=10; i++) {
+				
+				if (i==5) {
+					break;
+				}
 				System.out.println(i);
 			}
-			
+
 		}
 
 	}
-
-}
