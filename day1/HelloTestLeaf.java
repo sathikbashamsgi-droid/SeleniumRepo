34fa849a32fcb609c67a532a17f5b3fd5499d622
Sathik basha <sathikbashamsgi@gmail.com>
2025-08-12 14:36:29 +0530

    Add files via upload
    Week1

---
+++ day1/HelloTestLeaf.java
@@ -0,0 +1,11 @@
+package week1.day1;
+
+public class HelloTestLeaf {
+
+	public static void main(String[] args) {
+		
+		//how to print the statement--->type syso and do ctrl+space
+		System.out.println("welcome to testleaf");
+		
+	}
+}
